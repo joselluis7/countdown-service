1bbdb61d12152a29b1d4e5879f2dd579dfe605a7
Jose Luis <jluis@localhost.localdomain>
2020-03-30 02:33:23 +0100

    setting up files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # countdown-service
 A system with varios timer functions such as stopwatch, countdown
+
+The main goal of this project is to understanding some python resources
+Such as stdout, datetime, carriage return and go on
---
+++ app/countdown.py
@@ -0,0 +1,20 @@
+#-*-coding:utf-8
+
+#The main idea of this simple program is to understand the stdout
+#And carriage return functionality, both a old and little used 
+#But have specific uses like, as in this case
+ 
+from datetime import timedelta
+from sys import stdout
+from time import sleep
+ 
+time = input('Digite o tempo no seguinte formato [hh:mm:ss]: ').split(':')
+hours, minutes,seconds = time
+
+time = timedelta(hours=int(hours),minutes=int(minutes),seconds=int(seconds))
+
+while (str(time) != '00:00:00'):
+    stdout.write("\r%s "% (str(time)))
+    stdout.flush()
+    time = time - timedelta(seconds=(1/100))
+    sleep(1/100)
